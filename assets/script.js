288a6054f257cbfe0210e9150276350db6dea483
John Baxter <juancho420@ymail.com>
2021-04-03 22:56:07 -0700

    some pseudocode and some edits to html

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// add a header with the name of the app
+// hard code into html
+// will never change on the page and makes purpose of page clear
+
+// add 3 divs to the body of html to accept dynamic instructions from source files
+// with classes and id's 
+// so that window can display dynamically generated info
+
+// div1 contains a field requesting city name and submit button, as well as an empty column in which to store recents searches
+// text area captures city name value and button submits
+// to gain data from user with which to initiate search and then store that data on local storage displayed as recent searches
+
+// div2 is empty container in which response from querry is to be placed
+// with classes and/or ids to identify their location on document and jquerry
+// to provide user with requested information
+
+// div3 is empty container in which response from querry is to be placed
+// with classes and/or ids to identify their location on document and jquerry
+// to provide user with requested information
+
+// each div will be formatted and made dynamic with js framework other than bootstrap
+// this is to be determined
+// because I'm not quite there just yet
